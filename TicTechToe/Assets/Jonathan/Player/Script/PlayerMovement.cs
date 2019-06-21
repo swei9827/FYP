014e03bc796ea724a007788abecb949e980e3a9d
Jonhiew98 <0118562@kdu-online.com>
2019-06-21 23:02:43 +0800

    ggwp

---
+++ TicTechToe/Assets/Jonathan/Player/Script/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    public float sprintSpeed;
+    private Vector2 direction;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    void Move()
+    {
+        //before/after move
+        direction = Vector2.zero;
+
+        //movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+        }
+
+        //make player move
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+}
