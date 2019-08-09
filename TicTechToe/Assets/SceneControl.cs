9171c8711a1808da878af621b451021d924a6418
Jonhiew98 <0118562@kdu-online.com>
2019-08-09 21:36:29 +0800

    Main Menu

---
+++ TicTechToe/Assets/SceneControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+    public bool isTutorial;
+    public bool isGame;
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void Update()
+    {
+        if (isTutorial)
+        {
+            if(Input.anyKey)
+            {
+                SceneManager.LoadScene(2);
+            }
+        }
+
+        if(isGame)
+        {
+            if(Input.GetKeyDown(KeyCode.Escape))
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+}
