ea33850b507d86eb2012e70661ed71e0a2560eb3
Jonhiew98 <0118562@kdu-online.com>
2019-08-18 23:48:37 +0800

    Fix NPC

--- TicTechToe/Assets/Jonathan/Script/Others/NPCinteraction.cs
+++ TicTechToe/Assets/Jonathan/Script/Others/NPCinteraction.cs
@@ -42,12 +42,13 @@ public class NPCinteraction : MonoBehaviour
                 else
                 {
                     dialogueManager.NextSentence();
+                    Quests.SetActive(false);
                 }
             }
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Player"))
         {
