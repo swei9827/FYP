0156ca5559ed89078fee1e72afa1dfd3543d78c1
swei9827 <sw7177@gmail.com>
2019-09-10 06:28:44 +0800

    shop

---
+++ TicTechToe/Assets/SW/ShopSystem.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopSystem : MonoBehaviour
+{
+    int moneyAmount;
+
+    public Text currentMoney;
+    public Image[] itemIcon;
+    public Text[] itemPrice;
+    public Button[] buyButton;
+    public Item[] itemObject;
+
+    void Start()
+    {
+        moneyAmount = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInteraction>().playerCurrency;
+    }
+
+    private void OnEnable()
+    {
+        moneyAmount = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInteraction>().playerCurrency;
+    }
+
+    private void OnDisable()
+    {
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInteraction>().playerCurrency = moneyAmount;
+    }
+
+    void Update()
+    {
+        currentMoney.text = moneyAmount.ToString();
+    }
+
+    void buyItem()
+    {
+        moneyAmount -= 5;
+        //GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>().AddCropsItem(itemObject, CropsTypeTest,)
+    }
+
+    void buyItem(int item, int cost)
+    {
+        if(moneyAmount>=cost)
+        {
+            moneyAmount -= cost;
+        }
+        else
+        {
+            //GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>().AddCropsItem(itemObject, CropsTypeTest,)
+        }        
+    }
+
+    void closeShop()
+    {
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInteraction>().playerCurrency = moneyAmount;
+    }
+}
