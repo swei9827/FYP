00bf03f3c1844fd1a73ab78ef9eade3f51f7f1c7
zjLee <0118259@kdu-online.com>
2019-09-18 22:11:34 +0800

    Quest Manager Framework

---
+++ TicTechToe/Assets/ZJ/Quest/QuestInteraction.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestInteraction : MonoBehaviour
+{
+    public List<AcceptedQuests> acceptedQuestLists;
+
+    [System.Serializable]
+    public class AcceptedQuests
+    {
+        public string questName;
+        public int questID;        
+        public string requirement;
+        public int amount;
+        public int reward;
+        public bool accepted;
+        public bool completed;
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("NPC"))
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                AcceptedQuests aq = new AcceptedQuests();
+                if (!QuestManager.returnQuestStatus(collision.gameObject))
+                {
+                    aq.questName = QuestManager.returnQuestName(collision.gameObject);
+                    aq.questID = QuestManager.returnQuestID(collision.gameObject);
+                    aq.requirement = QuestManager.returnRequirement(collision.gameObject);
+                    aq.amount = QuestManager.returnAmount(collision.gameObject);
+                    aq.reward = QuestManager.returnReward(collision.gameObject);
+                    aq.accepted = true;
+                    QuestManager.questStatusChange(collision.gameObject);
+                    acceptedQuestLists.Add(aq);
+                }                
+            }
+        }
+    }
+}
---
+++ TicTechToe/Assets/ZJ/Quest/QuestManager.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    public static QuestManager instance;
+    public QuestInfo[] QuestsLists;
+
+    [System.Serializable]
+    public class QuestInfo
+    {
+        public int questID;
+        public string questName;
+        public GameObject QuestProvider;
+        public string questType;
+        public string questDetail;
+        public string objectName;
+        public int amount;
+        public int reward;
+        public bool accepted;
+        public bool completed;
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public static int returnQuestID(GameObject go)
+    {
+        QuestInfo q = Array.Find(instance.QuestsLists, QuestInfo => QuestInfo.QuestProvider == go);
+        if(q == null)
+        {
+            Debug.Log("Invalid NPC");
+            return -1;
+        }
+        else
+        {
+            return q.questID;
+        }
+    }
+
+    public static string returnRequirement(GameObject go)
+    {
+        QuestInfo q = Array.Find(instance.QuestsLists, QuestInfo => QuestInfo.QuestProvider == go);
+        if(q == null)
+        {
+            Debug.Log("Invalid NPC");
+            return "null";
+        }
+        else
+        {
+            return q.objectName;
+        }
+    }
+
+    public static int returnAmount(GameObject go)
+    {
+        QuestInfo q = Array.Find(instance.QuestsLists, QuestInfo => QuestInfo.QuestProvider == go);
+        if (q == null)
+        {
+            Debug.Log("Invalid NPC");
+            return -1;
+        }
+        else
+        {
+            return q.amount;
+        }
+    }
+
+    public static int returnReward(GameObject go)
+    {
+        QuestInfo q = Array.Find(instance.QuestsLists, QuestInfo => QuestInfo.QuestProvider == go);
+        if (q == null)
+        {
+            Debug.Log("Invalid NPC");
+            return -1;
+        }
+        else
+        {
+            return q.reward;
+        }
+    }
+
+    public static string returnQuestName(GameObject go)
+    {
+        QuestInfo q = Array.Find(instance.QuestsLists, QuestInfo => QuestInfo.QuestProvider == go);
+        if (q == null)
+        {
+            Debug.Log("Invalid NPC");
+            return "null";
+        }
+        else
+        {
+            return q.questName;
+        }
+    }
+
+    public static bool returnQuestStatus(GameObject go)
+    {
+        QuestInfo q = Array.Find(instance.QuestsLists, QuestInfo => QuestInfo.QuestProvider == go);
+        if (q == null)
+        {
+            Debug.Log("Invalid NPC");
+            return true;
+        }
+        else
+        {
+            return q.accepted;
+        }
+    }
+
+    public static void questStatusChange(GameObject go)
+    {
+        QuestInfo q = Array.Find(instance.QuestsLists, QuestInfo => QuestInfo.QuestProvider == go);
+        if (q == null)
+        {
+            Debug.Log("Invalid NPC");
+        }
+        else
+        {
+            q.accepted = true;
+        }
+    }
+}
