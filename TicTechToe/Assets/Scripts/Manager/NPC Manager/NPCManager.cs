d7b5ada0eb99a4042fa5ffeeef5161a862c48899
zjLee <0118259@kdu-online.com>
2019-09-27 19:13:57 +0800

    Merged Quest Manager into NPC Manager. Added NPC Manager. NPCs have a radius around to enable interaction and popup prompt.

---
+++ TicTechToe/Assets/Scripts/Manager/NPC Manager/NPCManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCManager : MonoBehaviour
+{
+    public GameObject popup;
+    public List<NPCs> npcList;
+    
+    Transform player;
+    NPCs currentNpc;
+    GameObject instance;
+    bool popupInstantiated;
+
+    [System.Serializable]
+    public class NPCs
+    {
+        public GameObject NPC;
+        public float distance;
+        public float offsetX;
+        public float offsetY;
+    }
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;        
+    }
+
+    void Awake()
+    {
+        StartCoroutine(DistanceCheck(.5f));
+    }
+
+    IEnumerator DistanceCheck(float time)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(time);
+            Debug.Log("x");
+            foreach (NPCs n in npcList)
+            {
+                float dist = Vector2.Distance(n.NPC.transform.position, player.position);
+
+                Vector2 parent = n.NPC.transform.position;
+                parent.x += n.offsetX;
+                parent.y += n.offsetY;
+
+                if (dist <= n.distance && !popupInstantiated)
+                {
+                    instance = Instantiate(popup, parent, Quaternion.identity);
+                    currentNpc = n;
+                    popupInstantiated = true;
+                }
+            }
+
+            if (currentNpc != null && Vector2.Distance(currentNpc.NPC.transform.position, player.position) > currentNpc.distance)
+            {
+                Destroy(instance);
+                popupInstantiated = false;
+                currentNpc = null;
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        foreach (NPCs n in npcList)
+        {
+            Vector2 pos = n.NPC.transform.position;
+            pos.x += n.offsetX;
+            pos.y += n.offsetY;
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawWireSphere(n.NPC.transform.position, n.distance);
+            Gizmos.DrawWireCube(pos, new Vector2(.5f,.5f));
+        }
+    }
+}
