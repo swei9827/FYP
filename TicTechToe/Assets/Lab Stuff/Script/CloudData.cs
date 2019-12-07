32f89244d940739de0825d8af7f3ade8ec7228c1
zjLee <0118259@kdu-online.com>
2019-12-07 13:46:30 +0800

    Cloud Inventory

--- TicTechToe/Assets/Lab Stuff/Script/CloudData.cs
+++ TicTechToe/Assets/Lab Stuff/Script/CloudData.cs
@@ -8,32 +8,18 @@ public class CloudData : MonoBehaviour
 {
     string MyPlayfabID;
     int currentDataVal;
+    int itemID;
+    int itemAmount;
+    bool playerDataReady = false;
+
+    Inventory inv;
 
     void Start()
     {
         GetAccountInfoRequest acc = new GetAccountInfoRequest();
         PlayFabClientAPI.GetAccountInfo(acc, success, fail);
-    }
-    
-    void Update()
-    {
-
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            ClientGetUserPublisherData("FishCount");
-            StartCoroutine(waitAndWriteData("FishCount", 1));
-        }
-
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            ClientSetUserPublisherData("Coins", 0);
-            ClientSetUserPublisherData("Catfish", 0);
-            ClientSetUserPublisherData("Potato", 0);
-            ClientSetUserPublisherData("Pumpkin", 0);
-            ClientSetUserPublisherData("Salmon", 0);
-            ClientSetUserPublisherData("Strawberry", 0);
-            ClientSetUserPublisherData("Tuna", 0);
-        }
+        inv = GameObject.Find("Inventory").GetComponent<Inventory>();
+        StartCoroutine(InitializeInventory());
     }
 
     public void ClientSetUserPublisherData(string dataName, int incre)
@@ -62,11 +48,11 @@ public class CloudData : MonoBehaviour
         }, result => {
             if (result.Data == null || !result.Data.ContainsKey(dataName))
             {
-                Debug.Log("No" + dataName);
+                //Debug.Log("No " + dataName);
             }
             else
             {
-                Debug.Log(dataName + " : " + result.Data[dataName].Value);
+                //Debug.Log(dataName + " : " + result.Data[dataName].Value);
                 currentDataVal = int.Parse(result.Data[dataName].Value);
             }
         },
@@ -76,10 +62,50 @@ public class CloudData : MonoBehaviour
         });
     }
 
-    IEnumerator waitAndWriteData(string dataName, int incre)
+    void retrieveInvData(string dataID)
+    {
+        PlayFabClientAPI.GetUserPublisherData(new GetUserDataRequest()
+        {
+            PlayFabId = MyPlayfabID
+        }, result => {
+            if (result.Data == null || !result.Data.ContainsKey(dataID))
+            {
+                Debug.Log("No " + "ItemID_" + dataID);
+            }
+            else
+            {
+                Debug.Log("ItemID_" + dataID + " " + inv.databaseRef.database[int.Parse(dataID)].itemName + " : " + result.Data[dataID].Value);
+                itemAmount = int.Parse(result.Data[dataID].Value);
+                itemID = int.Parse(dataID);
+
+                inv.AddItemFromCloud(itemID, itemAmount);
+            }
+        },
+        error => {
+            Debug.Log("Got error getting Regular Publisher Data:");
+            Debug.Log(error.GenerateErrorReport());
+        });
+    }  
+
+    public void writeToCloud(string dataID, int incre)
+    {
+        StartCoroutine(waitAndWriteData(dataID, incre));
+    }
+
+    IEnumerator waitAndWriteData(string dataID, int incre)
+    {
+        ClientGetUserPublisherData(dataID);
+        yield return new WaitForSeconds(1.0f);
+        ClientSetUserPublisherData(dataID, incre);
+    }
+
+    IEnumerator InitializeInventory()
     {
         yield return new WaitForSeconds(1.0f);
-        ClientSetUserPublisherData(dataName, incre);
+        for (int i = 0; i < inv.databaseRef.database.Count; i++)
+        {
+            retrieveInvData(i.ToString());
+        }
     }
 
     void success(GetAccountInfoResult result)
---
+++ TicTechToe/Assets/Lab Stuff/Script/UpdateDataRecord.cs
@@ -0,0 +1,60 @@
+﻿using PlayFab;
+using PlayFab.ClientModels;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using PlayFab.DataModels;
+using PlayFab.ProfilesModels;
+
+public class UpdateDataRecord : MonoBehaviour
+{
+    public bool allowDataTransfer;
+
+    string playerName;
+
+    void Start()
+    {
+        GetAccountInfo();
+    }
+
+    void GetAccountInfo()
+    {
+        GetAccountInfoRequest request = new GetAccountInfoRequest();
+        PlayFabClientAPI.GetAccountInfo(request, Successs, fail);
+    }
+
+
+    void Successs(GetAccountInfoResult result)
+    {
+        playerName = result.AccountInfo.Username;
+    }
+
+
+    void fail(PlayFabError error)
+    {
+        Debug.LogError(error.GenerateErrorReport());
+    }
+
+    IEnumerator PostData(string playername, string eventData)
+    {
+        WWWForm form = new WWWForm();
+
+        form.AddField("entry.1877185558", playername);
+        form.AddField("entry.1742081839", eventData);
+
+        byte[] rawData = form.data; ;
+
+        // Post a request to an URL with our custom headers
+        WWW www = new WWW("https://docs.google.com/forms/u/0/d/e/1FAIpQLScX-M_byLNBwyMF9IjfgTHv7l3IDC47tmzheJBslG9MUF2LXQ/formResponse", rawData);
+        yield return www;
+    }
+
+    public void sendData(string eventData)
+    {
+        if (allowDataTransfer)
+        {
+            StartCoroutine(PostData(playerName, eventData));
+        }
+    }
+}
