841b2c1fd72188f8ae857ba9b68e20bba09a2b6d
zjLee <0118259@kdu-online.com>
2019-12-09 01:59:51 +0800

    .

---
+++ TicTechToe/Assets/Lab Stuff/Script/DisplayUserName.cs
@@ -0,0 +1,42 @@
+﻿using PlayFab;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using PlayFab.ClientModels;
+using PlayFab.MultiplayerModels;
+
+public class DisplayUserName : MonoBehaviour
+{
+    public string displayName;
+
+    public bool showUserName;
+    public GameObject usernameDisplay;
+
+
+    void Start()
+    {
+        GetAccountInfoRequest acc = new GetAccountInfoRequest();
+        PlayFabClientAPI.GetAccountInfo(acc, success, fail);
+    }
+
+    void Update()
+    {
+        if (showUserName)
+        {
+            usernameDisplay.GetComponent<Text>().text = displayName;
+        }
+    }
+   
+    void success(GetAccountInfoResult result)
+    {
+        displayName = result.AccountInfo.Username;
+    }
+
+
+    void fail(PlayFabError error)
+    {
+        Debug.LogError(error.GenerateErrorReport());
+    }
+}
+
