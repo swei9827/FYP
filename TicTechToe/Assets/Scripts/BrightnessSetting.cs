f46757be77a3a141ca816fe9b7dee8539982db4e
Jonhiew98 <0118562@kdu-online.com>
2019-12-10 03:46:02 +0800

    Brightness settings

---
+++ TicTechToe/Assets/Scripts/BrightnessSetting.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BrightnessSetting : MonoBehaviour
+{
+    public static BrightnessSetting instance;
+
+    public Image brightnessImage;
+    private Color tempColor;
+    private GameObject temp;
+
+    private Slider menuBrightnessSlider;
+    private Slider gameBrightnessSlider;
+
+    private GameObject Canvas;
+
+    private Scene currentScene;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        brightnessImage = this.GetComponentInChildren<Image>();
+        tempColor = brightnessImage.color;
+    }
+
+    public void setBrightness(float value)
+    {
+        tempColor = brightnessImage.color;
+        tempColor.a = 1- value;
+        gameObject.transform.GetChild(0).GetComponent<Image>().color = tempColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
